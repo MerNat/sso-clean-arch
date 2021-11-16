0ea0f7c05a957646f542dc2f4adeece9144bef41
Meron <mernat777@gmail.com>
2021-11-16 19:35:59 +0300

    feat: sso test

--- usecase/sso/sso_test.go
+++ usecase/sso/sso_test.go
@@ -1,3 +1,23 @@
 package sso
 
+import (
+	"testing"
 
+	"github.com/mernat/sso-clean-arch/models"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_serviceLayer_RegistrationService(t *testing.T) {
+	u := &models.User{
+		Name: "AnyName",
+		Email: "anyemail@gmail.com",
+		Password: "mypassword",
+	}
+
+	ssoRepo := models.NewInmemRepository()
+	ssoUseCase := NewService(ssoRepo)
+
+	err := ssoUseCase.RegistrationService(u)
+
+	assert.Nil(t, err)
+}
